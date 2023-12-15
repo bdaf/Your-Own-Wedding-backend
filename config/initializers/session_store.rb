2ce073639289f3e87bf0904977c9bd8330a9a1a4
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-15 15:07:41 +0100

    Configured session_store to make it works on development environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication", domain: "kamil-your-own-wedding-app-api.herokuapp.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication", domain: "kamil-your-own-wedding-app-api.herokuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication"
+end
