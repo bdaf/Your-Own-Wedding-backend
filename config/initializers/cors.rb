6c26e946dc07326d6cd45ba465653c29896d49c3
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-15 14:26:27 +0100

    Corrected origins keyword

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origin "http://localhost:5173"
+        origins "http://localhost:5173"
         resource "*", 
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
-        origin "http://kamil-your-own-wedding-react.herokuapp.com"
+        origins "http://kamil-your-own-wedding-react.herokuapp.com"
         resource "*", 
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
