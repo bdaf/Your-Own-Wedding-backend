ea7faf71e0598dd4db8ba998d738a382f9b55e8a
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-15 14:48:17 +0100

    Login and register actions implemented

---
+++ db/migrate/20231215132847_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :surname
+      t.string :password_digest
+      t.integer :role
+      t.string :email
+      t.string :city
+      t.string :phone_number
+      t.datetime :celebration_date
+
+      t.timestamps
+    end
+  end
+end
