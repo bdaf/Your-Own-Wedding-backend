1cf3633539a9d7287b263c08c3e393ac914e507b
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-20 19:29:16 +0100

    Fast refactor of 3 just written tests

--- test/models/offer_test.rb
+++ test/models/offer_test.rb
@@ -9,10 +9,12 @@ class OfferTest < ActiveSupport::TestCase
   end
   
   test "should create offer with image" do
+    # given
     offer = Offer.new(address: @offer.address, description: @offer.description, title: @offer.title, user_id: @supportUser.id)
-    images_before = offer.images
     assert_not offer.images.attached?
+    # when
     offer.images.attach(io: File.open('test/fixtures/files/matka-boza-bolesna.jpg'), filename: 'filename')
+    # then
     assert offer.images.attached?
     assert offer.save!
     assert offer.id
@@ -20,13 +22,14 @@ class OfferTest < ActiveSupport::TestCase
   end
 
   test "should create offer with images" do
+    # given
     offer = Offer.new(address: @offer.address, description: @offer.description, title: @offer.title, user_id: @supportUser.id)
-    images_before = offer.images
     assert_not offer.images.attached?
-    debugger
+    # when
     offer.images.attach(io: File.open('test/fixtures/files/matka-boza-bolesna.jpg'), filename: 'filename')
     offer.images.attach(io: File.open('test/fixtures/files/matka-boza-bolesna.jpg'), filename: 'filename')
     offer.images.attach(io: File.open('test/fixtures/files/matka-boza-bolesna.jpg'), filename: 'filename')
+    # then
     assert offer.images.attached?
     assert offer.save!
     assert offer.id
@@ -34,7 +37,10 @@ class OfferTest < ActiveSupport::TestCase
   end
 
   test "should create offer without image" do
+    # given
+    # when
     offer = @supportUser.offers.create(address: @offer.address, description: @offer.description, title: @offer.title)
+    # then
     assert offer
     assert offer.id
     assert_equal(offer.images.attachments.count, 0)
