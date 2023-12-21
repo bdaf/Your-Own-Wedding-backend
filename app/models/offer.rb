aedd4232d3afeb8f896d1ffb73bd5463cf8895ff
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-21 01:39:52 +0100

    Added presence validation of user in offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,6 @@ class Offer < ApplicationRecord
     belongs_to :user
     has_many_attached :images
 
-    validates :title, :description, :address, presence: true
+    validates :title, :description, :address, :user, presence: true
     # attachable.variant :thumb, resize_to_limit: [100, 100]
 end
