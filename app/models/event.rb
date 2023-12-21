c7889abe475ca4aafb6c329d4ec54539acb3e6be
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-21 01:38:36 +0100

    Written three tests with presents validation of date, name and user in event and they pass

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :notes, dependent: :destroy
+
+  validates :date, :name, :user, presence: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @event = events(:one)
+    @clientUser = users(:client)
+    @supportUser = users(:support)
+  end
+  
+  test "should not create event without name" do
+    # given and when
+    event = @supportUser.events.create(date: @event.date)
+    # then
+    assert_not event.valid?
+    assert event.errors[:name].any?
+  end
+
+  test "should not create event without date" do
+    # given and when
+    event = @supportUser.events.create(name: @event.name)
+    # then
+    assert_not event.valid?
+    assert event.errors[:date].any?
+  end
+
+  test "should not create event without user" do
+    # given and when
+    event = Event.create(date: @event.date)
+    # then
+    assert_not event.valid?
+    assert event.errors[:user].any?
+  end
 end
