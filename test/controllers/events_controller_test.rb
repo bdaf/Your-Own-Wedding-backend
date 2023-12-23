058c7696b19348386bfe2814a5e5b529ce2eaf4c
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-23 01:43:41 +0100

    Updated controller destroy, update and show tests with authentication regarding events

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class EventsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @event = events(:one)
+    @supportUser = users(:support)
+    @clientUser = users(:client)
   end
 
   test "should get index" do
@@ -18,17 +20,59 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert_response :created
   end
 
-  test "should show event" do
+  test "should not show event if not logged in" do
+    get event_url(@event), as: :json
+    assert_response 401
+  end
+
+  test "should not show event if logged in as a not support" do
+    sign_in_as @clientUser, "12341234"
+    get event_url(@event), as: :json
+    assert_response 403
+  end
+
+  test "should show event if logged in as a support" do
+    sign_in_as @supportUser, "12341234"
     get event_url(@event), as: :json
     assert_response :success
   end
 
-  test "should update event" do
+  test "should not update when not logged in" do
+    patch event_url(@event), params: { event: { date: @event.date, name: @event.name, user_id: @event.user_id } }, as: :json
+    assert_response 401
+  end
+
+  test "should not update event when logged as a not support" do
+    sign_in_as @clientUser, "12341234"
+    patch event_url(@event), params: { event: { date: @event.date, name: @event.name, user_id: @event.user_id } }, as: :json
+    assert_response 403
+  end
+
+  test "should update event when logged in as a support" do
+    sign_in_as @supportUser, "12341234"
     patch event_url(@event), params: { event: { date: @event.date, name: @event.name, user_id: @event.user_id } }, as: :json
     assert_response :success
   end
 
-  test "should destroy event" do
+  test "should not destroy if not logged in" do
+    assert_difference("Event.count", 0) do
+      delete event_url(@event), as: :json
+    end
+
+    assert_response 401
+  end
+
+  test "should not destroy event if logged in as a not support" do
+    sign_in_as @clientUser, "12341234"
+    assert_difference("Event.count", 0) do
+      delete event_url(@event), as: :json
+    end
+
+    assert_response 403
+  end
+
+  test "should destroy event if logged in as a support" do
+    sign_in_as @supportUser, "12341234"
     assert_difference("Event.count", -1) do
       delete event_url(@event), as: :json
     end
