5dba32bfca2774cec8f14642d4aa528ed5405b3a
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-28 00:06:52 +0100

    Tests in guest model regarding presence passes

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,6 @@
 class Guest < ApplicationRecord
   belongs_to :user
   has_many :addition_attribiutes, dependent: :destroy
+
+  validates :name, :surname, :phone_number, presence: true
 end
--- test/models/guest_test.rb
+++ test/models/guest_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class GuestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @guest = guests(:one)
+    @clientUser = users(:client)
+    @secondClientUser = users(:client_2)
+    @supportUser = users(:support)
+  end
+
+  test "should not create guest without user" do
+    # given and when
+    guest = Guest.create(name: @guest.name, surname: @guest.surname, phone_number: @guest.phone_number);
+    # then
+    assert_not guest.valid?
+    assert guest.errors[:user].any?
+  end
+
+  test "should not create guest without name" do
+    # given and when
+    guest = @clientUser.guests.create(surname: @guest.surname, phone_number: @guest.phone_number);
+    # then
+    assert_not guest.valid?
+    assert guest.errors[:name].any?
+  end
+
+  test "should not create guest without surname" do
+    # given and when
+    guest = @clientUser.guests.create(name: @guest.name, phone_number: @guest.phone_number);
+    # then
+    assert_not guest.valid?
+    assert guest.errors[:surname].any?
+  end
+
+  test "should not create guest without phone number" do
+    # given and when
+    guest = @clientUser.guests.create(name: @guest.name, surname: @guest.surname);
+    # then
+    assert_not guest.valid?
+    assert guest.errors[:phone_number].any?
+  end
+  
+  
 end
