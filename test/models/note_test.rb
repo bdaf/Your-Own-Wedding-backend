2b7addfb7f1d62be8f77473a615f5a21d66f198a
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-27 16:13:53 +0100

    Added tests about presence of name, body and events in note model and they pass

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @event = events(:one)
+    @event_two = events(:two)
+    @note = notes(:one)
+  end
+  
+  test "should create note through event with name and body" do
+    # given and when
+    note = @event.notes.create(name: @note.name, body: @note.body)
+    # then
+    assert note.valid?
+  end
+
+  test "should not create note without event" do
+    # given and when
+    note = Note.create(name: @note.name, body: @note.body)
+    # then
+    assert_not note.valid?
+    assert note.errors[:event].any?
+  end
+
+  test "should not create note without name" do
+    # given and when
+    note = @event.notes.create(body: @note.body)
+    # then
+    assert_not note.valid?
+    assert note.errors[:name].any?
+  end
+
+  test "should create note without body" do
+    # given and when
+    note = @event.notes.create(name: @note.name)
+    # then
+    assert_not note.valid?
+    assert note.errors[:body].any?
+  end
 end
