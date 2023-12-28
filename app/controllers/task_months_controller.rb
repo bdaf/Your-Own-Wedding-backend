fd5d02e47697b56ae8957450f9760703f3dbe59e
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-28 17:31:24 +0100

    Integration tests for task months are done

--- app/controllers/task_months_controller.rb
+++ app/controllers/task_months_controller.rb
@@ -1,37 +1,24 @@
 class TaskMonthsController < ApplicationController
-  before_action :set_task_month, only: %i[ show update destroy ]
+  include CurrentUserConcern
+  before_action :authenticate_as_client
+  before_action :set_task_month, only: %i[ show destroy ]
 
+  # Don't need for now
   # GET /task_months
   # GET /task_months.json
-  def index
+  # def index
+  #   @task_months = TaskMonth.all
+  # end
+
+  # GET /task_months_my
+  # GET /task_months_my.json
+  def my
     @task_months = TaskMonth.all
   end
 
   # GET /task_months/1
   # GET /task_months/1.json
   def show
-  end
-
-  # POST /task_months
-  # POST /task_months.json
-  def create
-    @task_month = TaskMonth.new(task_month_params)
-
-    if @task_month.save
-      render :show, status: :created, location: @task_month
-    else
-      render json: @task_month.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /task_months/1
-  # PATCH/PUT /task_months/1.json
-  def update
-    if @task_month.update(task_month_params)
-      render :show, status: :ok, location: @task_month
-    else
-      render json: @task_month.errors, status: :unprocessable_entity
-    end
   end
 
   # DELETE /task_months/1
@@ -44,10 +31,11 @@ class TaskMonthsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_task_month
       @task_month = TaskMonth.find(params[:id])
+      render_forbidden_if_not_users_object @task_month
     end
 
     # Only allow a list of trusted parameters through.
     def task_month_params
-      params.require(:task_month).permit(:user_id, :month_number, :year)
+      params.require(:task_month).permit(:month_number, :year)
     end
 end
--- test/controllers/task_months_controller_test.rb
+++ test/controllers/task_months_controller_test.rb
@@ -3,34 +3,105 @@ require "test_helper"
 class TaskMonthsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @task_month = task_months(:one)
+    @supportUser = users(:support)
+    @clientUser = users(:client)
+    @client_2_User = users(:client_2)
+  end
+  # Don't need for now
+  # test "should get index" do
+  #   get task_months_url, as: :json
+  #   assert_response :success
+  # end
+
+  test "should not get my task_months when not logged in" do
+    get my_task_months_url, as: :json
+    assert_response 401
   end
 
-  test "should get index" do
-    get task_months_url, as: :json
+  test "should not get my task_months when logged in as support" do
+    sign_in_as @supportUser, "12341234"
+    get my_task_months_url, as: :json
+    assert_response 403
+  end
+
+  test "should get my task_months" do
+    sign_in_as @clientUser, "12341234"
+    get my_task_months_url, as: :json
     assert_response :success
   end
 
-  test "should create task_month" do
-    assert_difference("TaskMonth.count") do
-      post task_months_url, params: { task_month: { month_number: @task_month.month_number, user_id: @task_month.user_id, year: @task_month.year } }, as: :json
-    end
-
-    assert_response :created
+  test "should get my task_months with tasks" do
+    sign_in_as @clientUser, "12341234"
+    get my_task_months_url, as: :json
+    assert_response :success
+    assert_match "tasks", @response.body
   end
 
-  test "should show task_month" do
+  test "should not show task_monthh when not logged in" do
+    get task_month_url(@task_month), as: :json
+    assert_response 401
+  end
+
+  test "should not show task_month when logged in as support" do
+    sign_in_as @supportUser, "12341234"
+    get task_month_url(@task_month), as: :json
+    assert_response 403
+  end
+
+  test "should not show task_month when logged in as not owner" do
+    sign_in_as @client_2_User, "12341234"
+    get task_month_url(@task_month), as: :json
+    assert_response 403
+  end
+
+  test "should show task_month when logged in as not owner" do
+    sign_in_as @clientUser, "12341234"
     get task_month_url(@task_month), as: :json
     assert_response :success
   end
 
-  test "should update task_month" do
-    patch task_month_url(@task_month), params: { task_month: { month_number: @task_month.month_number, user_id: @task_month.user_id, year: @task_month.year } }, as: :json
-    assert_response :success
+  test "should not destroy task_month when not logged in" do
+    assert_difference("TaskMonth.count", 0) do
+      delete task_month_url(@task_month), as: :json
+    end
+
+    assert_response 401
+  end
+
+  test "should not destroy task_month when logged in as support" do
+    sign_in_as @supportUser, "12341234"
+    assert_difference("TaskMonth.count", 0) do
+      delete task_month_url(@task_month), as: :json
+    end
+
+    assert_response 403
+  end
+
+  test "should not destroy task_month when logged in as not owner" do
+    sign_in_as @client_2_User, "12341234"
+    assert_difference("TaskMonth.count", 0) do
+      delete task_month_url(@task_month), as: :json
+    end
+
+    assert_response 403
   end
 
   test "should destroy task_month" do
+    sign_in_as @clientUser, "12341234"
     assert_difference("TaskMonth.count", -1) do
       delete task_month_url(@task_month), as: :json
+    end
+
+    assert_response :no_content
+  end
+
+  test "should destroy task_month also with tasks" do
+    sign_in_as @clientUser, "12341234"
+    assert @task_month.tasks.count == 1
+    assert_difference("TaskMonth.count", -1) do
+      assert_difference("Task.count", -1) do
+        delete task_month_url(@task_month), as: :json
+      end 
     end
 
     assert_response :no_content
