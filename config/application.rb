5eb37bc8ad44e4fdcb531c167eebfad5d5e4c4d2
bdaf <kamilkorenkiewiczpw@gmail.com>
2024-01-30 20:49:08 +0100

    Timezone changed to 'Warsaw' (+1)

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module YourOwnWedding
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Warsaw"
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.api_only = true
