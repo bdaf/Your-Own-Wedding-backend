99ca93ab91ba3659e7516c536ed44fadc60fb132
bdaf <kamilkorenkiewiczpw@gmail.com>
2024-02-01 12:39:47 +0100

    Added main page

--- config/routes.rb
+++ config/routes.rb
@@ -32,5 +32,6 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "/" => "rails/health#show", as: :main
   get "up" => "rails/health#show", as: :rails_health_check
 end
