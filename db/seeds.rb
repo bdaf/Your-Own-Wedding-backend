a47b26cfaccf1b96c74be9f0c4d57854435a8fc7
bdaf <kamilkorenkiewiczpw@gmail.com>
2024-02-15 15:15:49 +0100

    Seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,22 @@
 
 
 def create_main_event_with_example_note user
+    debugger
     user.events.create(date: Time.now + 1.minute, name: "Created account - space for flexible notes")
     user.events.first.notes.create(name: "Example of note", body: "You can create notes like this one.")
 end
-
 User.destroy_all
 provider = Provider.new(address: "Białystok", phone_number: "997997997")
 organizer = Organizer.new(celebration_date: Time.now + 2.years)
-user = User.create(email: "OrganizerUser@yow.pl", password: "Qwert123@", password_confirmation: "Qwert123@", role: "organizer", organizer: organizer);
-providerUser = User.create(email: "ProviderUser@yow.pl", password: "Qwert123@", password_confirmation: "Qwert123@", role: "provider", provider: provider);
+user = User.create(email: "OrganizerUser@yow.pl", password: "Qwert123$", password_confirmation: "Qwert123@", role: "organizer", organizer: organizer);
+providerUser = User.create(email: "ProviderUser@yow.pl", password: "Qwert123$", password_confirmation: "Qwert123@", role: "provider", provider: provider);
 
 create_main_event_with_example_note user
 create_main_event_with_example_note providerUser
 providerUser.events.create(date: Time.now + 1.minute, name: "Wedding of Kowalscy")
 providerUser.events.first.notes.create(name: "Cena", body: "Mieli zapłącić 8000 zł")
 
-20.times do 
+100.times do 
     providerUser.provider.offers.create(
         title: Faker::Lorem.sentence(word_count: 3),
         description: Faker::Lorem.paragraph(sentence_count: 30),
